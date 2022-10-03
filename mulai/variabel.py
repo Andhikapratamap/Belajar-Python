e4bf3d41c0fdc6f44393d66edf2a28d991b28948
Andhikaprt <107765982+Dhikaweb7@users.noreply.github.com>
2022-10-03 20:41:57 +0700

    Create variabel.py

---
+++ mulai/variabel.py
@@ -0,0 +1,35 @@
+
+#proses memasukan data ke dalam variabel
+nama = "Andhika Pratama Putra"
+#proses mencetak variabel
+print(nama)
+
+#nilai dan tipe data dalam variabel  dapat diubah
+umur = 20               #nilai awal
+print(umur)             #mencetak nilai umur
+type(umur)              #mengecek tipe data umur
+umur = "dua puluh satu" #nilai setelah diubah
+print(umur)             #mencetak nilai umur
+type(umur)              #mengecek tipe data umur
+
+namaDepan = "Diandra"
+namaBelakang = "naufal"
+nama = namaDepan + " " + namaBelakang
+umur = 18
+hobi = "Sepak bola"
+print("Biodata\n", nama, "\n", umur, "\n", hobi)
+
+#contoh variabel lainya
+inivariabel = "Halo"
+ini_juga_variabel = "Hai"
+_inivariabeljuga = "Hi"
+inivariabel222 = "Bye" 
+
+panjang = 10
+lebar = 5
+luas = panjang * lebar
+print(luas)
+
+
+
+
