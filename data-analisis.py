a6f7b1ce1846497e6f86dd8d0e567c89ab7cac96
Andhikaprt <107765982+Dhikaweb7@users.noreply.github.com>
2022-10-17 09:38:01 +0700

    Update data-analisis.py

--- data-analisis.py
+++ data-analisis.py
@@ -1,7 +1,11 @@
-
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+
+# Chika Mey Andela
+# Mata Pelajaran TIK | ANALISIS DATA
+# X TKJ 1
+# SMK RADEN PAKU
 
 th = "https://www.jobs.id/lowongan-kerja?kata-kunci=part time"
 halaman = requests.get(th)
